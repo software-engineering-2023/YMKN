d6f8979c8b7601b35d0bb03a6ff4b52ed6da498a
NadudaN <127606072+NadudaN@users.noreply.github.com>
2023-05-25 23:23:16 +0300

    Add files via upload
    updated account

--- M3/Admin/Account/account.js
+++ M3/Admin/Account/account.js
@@ -1,3 +1,32 @@
+         var AcceptBR = () => {
+        let client = document.getElementById('Clients2').value
+        console.log(client)
+        if (!client) {
+          return
+        }     
+          else if (client == '125882522, Clara') {
+            document.getElementById('AA').innerText = 'Client "Clara" with ID "125882522" is accepted' 
+          }
+          else if(client == "611213258, Nour"){
+            document.getElementById('AA').innerText = 'Client "Nour" with ID "611213258" is accepted' 
+          }
+        }
+          
 
-  if (username === "client" && password === "client") {
-    window.location.href = " ../Client/index.html";
+
+        
+        var RejectBR = () => {
+ 
+         {
+            
+        } 
+        var AcceptBL = () => {
+ 
+      
+        } 
+        var RejectBL = () => {
+          
+           
+           }
+        } 
+      
